fdacd6afed281f94b31ad6a1507464b735ba176f
jasalguero <jose.salguero@groupon.com>
2013-08-05 22:46:12 +0200

    added logical and async

--- app/async.js
+++ app/async.js
@@ -3,11 +3,30 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+        var dfd = new $.Deferred();
 
+        if (value === true){
+            setTimeout(dfd.resolve(value), 5000)
+        }else {
+            dfd.resolve(value);
+        }
+
+        return dfd;
     },
 
     manipulateRemoteData : function(url) {
+        var dfd = new $.Deferred();
 
+        $.get(url, function(data){
+            var result = [];
+            data.people.forEach(function(item){
+               result.push(item['name']);
+            });
+            result.sort();
+            dfd.resolve(result);
+        });
+
+        return dfd;
     }
   };
 });
