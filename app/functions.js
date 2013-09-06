5b92c3f3cd021a518ec031153f430c86338ab20e
jasalguero <jose.salguero@groupon.com>
2013-09-06 10:57:52 +0200

    started functions

--- app/functions.js
+++ app/functions.js
@@ -3,27 +3,35 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn.apply(this, arr);
     },
 
     speak : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+        return function(param){
+            return str + ', ' + param
+        }
     },
 
     makeClosures : function(arr, fn) {
-
+        var result = []
+        arr.forEach(function(elem){
+            result.push(function(){return fn(elem)});
+        });
+        return result;
     },
 
     partial : function(fn, str1, str2) {
-
+        return function(str3){
+            return fn(str1, str2, str3);
+        }
     },
 
     useArguments : function() {
-
+        return [].reduce.call(arguments, function(prev, current){return prev + current})
     },
 
     callIt : function(fn) {
